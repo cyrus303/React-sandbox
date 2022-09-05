8373252b4cca16517202f0e9c7e2623d614eda0d
cyrus303 <sachinsunny303@gmail.com>
2022-09-05 07:55:27 +0100

    app completed

--- WeatherApp/src/App.jsx
+++ WeatherApp/src/App.jsx
@@ -1,33 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
   const [data, setData] = useState({});
+  const [geoData, setGeoData] = useState({});
   const [location, setLocation] = useState('');
 
   const API_KEY = '43692b62367842e0a425fb6a98d28a5f';
 
-  const GEO_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=${API_KEY}`;
+  const GEO_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${
+    location ? location : 'Dublin'
+  }&limit=5&appid=${API_KEY}`;
+
+  function fetchData() {
+    axios.get(GEO_URL).then((response) => {
+      let lat = response.data[0].lat;
+      let lon = response.data[0].lon;
+      setGeoData(response.data);
+      const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`;
+
+      axios.get(URL).then((response) => {
+        setData(response.data);
+      });
+    });
+    setLocation('');
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   const searchLocation = () => {
     if (event.key === 'Enter') {
-      axios.get(GEO_URL).then((response) => {
-        let lat = response.data[0].lat;
-        let lon = response.data[0].lon;
-
-        // console.log(response.data);
-
-        const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-
-        axios.get(URL).then((response) => {
-          // console.log(response.data);
-          setData(response.data);
-        });
-      });
+      fetchData();
     }
   };
+  // todo build a default case
 
-  console.log(data);
+  // console.log(geoData[0]);
+
   return (
     <div className="App">
       <div className="search">
@@ -41,26 +52,30 @@ function App() {
       <div className="container">
         <div className="top">
           <div className="location">
-            <p>{location}</p>
+            {geoData.length > 0 ? <p>{geoData[0].name}</p> : null}
           </div>
           <div className="temp">
-            <h1>{}</h1>
+            {data.main ? <h1>{data.main.temp.toFixed()} °C</h1> : null}
           </div>
           <div className="description">
-            <p>clouds</p>
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
           </div>
         </div>
         <div className="bottom">
           <div className="feels">
-            <p className="bold">65 °F</p>
+            {data.main ? (
+              <p className="bold">{data.main.feels_like.toFixed()} °C</p>
+            ) : null}
             <p>Feels Liks</p>
           </div>
           <div className="humidity">
-            <p className="bold">20%</p>
+            {data.main ? <p className="bold">{data.main.humidity} %</p> : null}
             <p>Humidity</p>
           </div>
           <div className="wind">
-            <p className="bold">12 Kmph</p>
+            {data.main ? (
+              <p className="bold">{(data.wind.speed * 3.6).toFixed()} Kmph</p>
+            ) : null}
             <p>Winds</p>
           </div>
         </div>
