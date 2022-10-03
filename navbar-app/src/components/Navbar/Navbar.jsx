c5acd9ecf976bb66733fa31c643a4769aee19464
cyrus303 <sachinsunny303@gmail.com>
2022-10-03 20:15:46 +0100

    nav bar complete

--- navbar-app/src/components/Navbar/Navbar.jsx
+++ navbar-app/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Button } from '../Button';
 import { MenuItems } from '../MenuItems';
 
 import './Navbar.css';
@@ -16,9 +17,6 @@ function Navbar() {
         React <i className="fab fa-react"></i>
       </h1>
       <div className="menu-icon" onClick={handleClick}>
-        {/* <i
-          className={clicked ? 'fa-regular fa-bars' : 'fa-light fa-xmark-large'}
-        ></i> */}
         <i className={clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
       </div>
       <ul className={clicked ? 'nav-menu active' : 'nav-menu'}>
@@ -28,11 +26,11 @@ function Navbar() {
               <a className={item.cName} href={item.url}>
                 {item.title}
               </a>
-              {/* {console.log(item)} */}
             </li>
           );
         })}
       </ul>
+      <Button>Sign Up</Button>
     </nav>
   );
 }
